d6bc003e41b76e20bf772e298878c72de7debb50
nachomf <nachoemf@hotmail.com>
2014-10-15 21:15:28 -0300

    subo el ABM de Hotel

---
+++ FrbaHotel/FrbaHotel/ABM de Hotel/InHabilitarHotel.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace FrbaHotel.ABM_de_Hotel
+{
+    partial class InHabilitarHotel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDesde = new System.Windows.Forms.Label();
+            this.labelHasta = new System.Windows.Forms.Label();
+            this.labelMotivo = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.botonInhabilitar = new System.Windows.Forms.Button();
+            this.botonCancelar = new System.Windows.Forms.Button();
+            this.dateDesde = new System.Windows.Forms.DateTimePicker();
+            this.dateHasta = new System.Windows.Forms.DateTimePicker();
+            this.textBoxMotivo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelDesde
+            // 
+            this.labelDesde.AutoSize = true;
+            this.labelDesde.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDesde.Location = new System.Drawing.Point(12, 51);
+            this.labelDesde.Name = "labelDesde";
+            this.labelDesde.Size = new System.Drawing.Size(46, 15);
+            this.labelDesde.TabIndex = 0;
+            this.labelDesde.Text = "Desde:";
+            // 
+            // labelHasta
+            // 
+            this.labelHasta.AutoSize = true;
+            this.labelHasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHasta.Location = new System.Drawing.Point(12, 89);
+            this.labelHasta.Name = "labelHasta";
+            this.labelHasta.Size = new System.Drawing.Size(42, 15);
+            this.labelHasta.TabIndex = 1;
+            this.labelHasta.Text = "Hasta:";
+            // 
+            // labelMotivo
+            // 
+            this.labelMotivo.AutoSize = true;
+            this.labelMotivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMotivo.Location = new System.Drawing.Point(12, 125);
+            this.labelMotivo.Name = "labelMotivo";
+            this.labelMotivo.Size = new System.Drawing.Size(46, 15);
+            this.labelMotivo.TabIndex = 2;
+            this.labelMotivo.Text = "Motivo:";
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelTitulo.Location = new System.Drawing.Point(61, 9);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(161, 24);
+            this.labelTitulo.TabIndex = 3;
+            this.labelTitulo.Text = "Inhabilatar Hotel";
+            // 
+            // botonInhabilitar
+            // 
+            this.botonInhabilitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonInhabilitar.Location = new System.Drawing.Point(15, 175);
+            this.botonInhabilitar.Name = "botonInhabilitar";
+            this.botonInhabilitar.Size = new System.Drawing.Size(75, 23);
+            this.botonInhabilitar.TabIndex = 4;
+            this.botonInhabilitar.Text = "Inhabilitar";
+            this.botonInhabilitar.UseVisualStyleBackColor = true;
+            // 
+            // botonCancelar
+            // 
+            this.botonCancelar.Location = new System.Drawing.Point(210, 175);
+            this.botonCancelar.Name = "botonCancelar";
+            this.botonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.botonCancelar.TabIndex = 5;
+            this.botonCancelar.Text = "Cancelar";
+            this.botonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // dateDesde
+            // 
+            this.dateDesde.Location = new System.Drawing.Point(65, 51);
+            this.dateDesde.Name = "dateDesde";
+            this.dateDesde.Size = new System.Drawing.Size(220, 20);
+            this.dateDesde.TabIndex = 6;
+            // 
+            // dateHasta
+            // 
+            this.dateHasta.Location = new System.Drawing.Point(65, 89);
+            this.dateHasta.Name = "dateHasta";
+            this.dateHasta.Size = new System.Drawing.Size(220, 20);
+            this.dateHasta.TabIndex = 7;
+            // 
+            // textBoxMotivo
+            // 
+            this.textBoxMotivo.Location = new System.Drawing.Point(65, 125);
+            this.textBoxMotivo.Name = "textBoxMotivo";
+            this.textBoxMotivo.Size = new System.Drawing.Size(220, 20);
+            this.textBoxMotivo.TabIndex = 8;
+            // 
+            // InHabilitarHotel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 210);
+            this.Controls.Add(this.textBoxMotivo);
+            this.Controls.Add(this.dateHasta);
+            this.Controls.Add(this.dateDesde);
+            this.Controls.Add(this.botonCancelar);
+            this.Controls.Add(this.botonInhabilitar);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.labelMotivo);
+            this.Controls.Add(this.labelHasta);
+            this.Controls.Add(this.labelDesde);
+            this.Name = "InHabilitarHotel";
+            this.Text = "InHabilitarHotel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDesde;
+        private System.Windows.Forms.Label labelHasta;
+        private System.Windows.Forms.Label labelMotivo;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Button botonInhabilitar;
+        private System.Windows.Forms.Button botonCancelar;
+        private System.Windows.Forms.DateTimePicker dateDesde;
+        private System.Windows.Forms.DateTimePicker dateHasta;
+        private System.Windows.Forms.TextBox textBoxMotivo;
+    }
+}
---
+++ FrbaHotel/FrbaHotel/ABM de Hotel/ModificarHotel.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace FrbaHotel.ABM_de_Hotel
+{
+    partial class ModificarHotel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPais = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.listBoxRegimenes = new System.Windows.Forms.ListBox();
+            this.comboBoxTipoRegimen = new System.Windows.Forms.ComboBox();
+            this.dateFechaCreacion = new System.Windows.Forms.DateTimePicker();
+            this.textBoxCiudad = new System.Windows.Forms.TextBox();
+            this.textBoxTelefono = new System.Windows.Forms.TextBox();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.comboBoxCantEstrellas = new System.Windows.Forms.ComboBox();
+            this.textBoxNombreHotel = new System.Windows.Forms.TextBox();
+            this.botonAgregarRegimen = new System.Windows.Forms.Button();
+            this.botonVolver = new System.Windows.Forms.Button();
+            this.botonLimpiar = new System.Windows.Forms.Button();
+            this.botonRegistrar = new System.Windows.Forms.Button();
+            this.labelTipoRegimen = new System.Windows.Forms.Label();
+            this.labelFechaCreacion = new System.Windows.Forms.Label();
+            this.labelPais = new System.Windows.Forms.Label();
+            this.labelCiudad = new System.Windows.Forms.Label();
+            this.labelCantEstrellas = new System.Windows.Forms.Label();
+            this.labelDireccion = new System.Windows.Forms.Label();
+            this.labelTelefono = new System.Windows.Forms.Label();
+            this.labelMail = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelTituloDatos = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.botonQuitarRegimen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPais
+            // 
+            this.textBoxPais.Location = new System.Drawing.Point(242, 181);
+            this.textBoxPais.Name = "textBoxPais";
+            this.textBoxPais.Size = new System.Drawing.Size(96, 20);
+            this.textBoxPais.TabIndex = 49;
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.Location = new System.Drawing.Point(258, 152);
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(134, 20);
+            this.textBoxDireccion.TabIndex = 48;
+            // 
+            // listBoxRegimenes
+            // 
+            this.listBoxRegimenes.FormattingEnabled = true;
+            this.listBoxRegimenes.Location = new System.Drawing.Point(422, 108);
+            this.listBoxRegimenes.Name = "listBoxRegimenes";
+            this.listBoxRegimenes.Size = new System.Drawing.Size(199, 121);
+            this.listBoxRegimenes.TabIndex = 47;
+            // 
+            // comboBoxTipoRegimen
+            // 
+            this.comboBoxTipoRegimen.FormattingEnabled = true;
+            this.comboBoxTipoRegimen.Location = new System.Drawing.Point(422, 70);
+            this.comboBoxTipoRegimen.Name = "comboBoxTipoRegimen";
+            this.comboBoxTipoRegimen.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTipoRegimen.TabIndex = 46;
+            // 
+            // dateFechaCreacion
+            // 
+            this.dateFechaCreacion.Location = new System.Drawing.Point(126, 214);
+            this.dateFechaCreacion.Name = "dateFechaCreacion";
+            this.dateFechaCreacion.Size = new System.Drawing.Size(224, 20);
+            this.dateFechaCreacion.TabIndex = 45;
+            // 
+            // textBoxCiudad
+            // 
+            this.textBoxCiudad.Location = new System.Drawing.Point(63, 176);
+            this.textBoxCiudad.Name = "textBoxCiudad";
+            this.textBoxCiudad.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCiudad.TabIndex = 44;
+            // 
+            // textBoxTelefono
+            // 
+            this.textBoxTelefono.Location = new System.Drawing.Point(72, 148);
+            this.textBoxTelefono.Name = "textBoxTelefono";
+            this.textBoxTelefono.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTelefono.TabIndex = 43;
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(48, 122);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(158, 20);
+            this.textBoxMail.TabIndex = 42;
+            // 
+            // comboBoxCantEstrellas
+            // 
+            this.comboBoxCantEstrellas.FormattingEnabled = true;
+            this.comboBoxCantEstrellas.Location = new System.Drawing.Point(140, 93);
+            this.comboBoxCantEstrellas.Name = "comboBoxCantEstrellas";
+            this.comboBoxCantEstrellas.Size = new System.Drawing.Size(45, 21);
+            this.comboBoxCantEstrellas.TabIndex = 41;
+            // 
+            // textBoxNombreHotel
+            // 
+            this.textBoxNombreHotel.Location = new System.Drawing.Point(120, 67);
+            this.textBoxNombreHotel.Name = "textBoxNombreHotel";
+            this.textBoxNombreHotel.Size = new System.Drawing.Size(161, 20);
+            this.textBoxNombreHotel.TabIndex = 40;
+            // 
+            // botonAgregarRegimen
+            // 
+            this.botonAgregarRegimen.Location = new System.Drawing.Point(549, 68);
+            this.botonAgregarRegimen.Name = "botonAgregarRegimen";
+            this.botonAgregarRegimen.Size = new System.Drawing.Size(75, 23);
+            this.botonAgregarRegimen.TabIndex = 39;
+            this.botonAgregarRegimen.Text = "Agregar";
+            this.botonAgregarRegimen.UseVisualStyleBackColor = true;
+            // 
+            // botonVolver
+            // 
+            this.botonVolver.Location = new System.Drawing.Point(551, 279);
+            this.botonVolver.Name = "botonVolver";
+            this.botonVolver.Size = new System.Drawing.Size(75, 23);
+            this.botonVolver.TabIndex = 38;
+            this.botonVolver.Text = "Volver";
+            this.botonVolver.UseVisualStyleBackColor = true;
+            // 
+            // botonLimpiar
+            // 
+            this.botonLimpiar.Location = new System.Drawing.Point(110, 279);
+            this.botonLimpiar.Name = "botonLimpiar";
+            this.botonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.botonLimpiar.TabIndex = 37;
+            this.botonLimpiar.Text = "Limpiar";
+            this.botonLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // botonRegistrar
+            // 
+            this.botonRegistrar.Location = new System.Drawing.Point(10, 279);
+            this.botonRegistrar.Name = "botonRegistrar";
+            this.botonRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.botonRegistrar.TabIndex = 36;
+            this.botonRegistrar.Text = "Registrar";
+            this.botonRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // labelTipoRegimen
+            // 
+            this.labelTipoRegimen.AutoSize = true;
+            this.labelTipoRegimen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTipoRegimen.Location = new System.Drawing.Point(311, 70);
+            this.labelTipoRegimen.Name = "labelTipoRegimen";
+            this.labelTipoRegimen.Size = new System.Drawing.Size(105, 15);
+            this.labelTipoRegimen.TabIndex = 35;
+            this.labelTipoRegimen.Text = "Tipo de Regimen:";
+            // 
+            // labelFechaCreacion
+            // 
+            this.labelFechaCreacion.AutoSize = true;
+            this.labelFechaCreacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFechaCreacion.Location = new System.Drawing.Point(7, 214);
+            this.labelFechaCreacion.Name = "labelFechaCreacion";
+            this.labelFechaCreacion.Size = new System.Drawing.Size(113, 15);
+            this.labelFechaCreacion.TabIndex = 34;
+            this.labelFechaCreacion.Text = "Fecha de Creacion:";
+            // 
+            // labelPais
+            // 
+            this.labelPais.AutoSize = true;
+            this.labelPais.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPais.Location = new System.Drawing.Point(202, 182);
+            this.labelPais.Name = "labelPais";
+            this.labelPais.Size = new System.Drawing.Size(34, 15);
+            this.labelPais.TabIndex = 33;
+            this.labelPais.Text = "Pais:";
+            // 
+            // labelCiudad
+            // 
+            this.labelCiudad.AutoSize = true;
+            this.labelCiudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCiudad.Location = new System.Drawing.Point(7, 182);
+            this.labelCiudad.Name = "labelCiudad";
+            this.labelCiudad.Size = new System.Drawing.Size(49, 15);
+            this.labelCiudad.TabIndex = 32;
+            this.labelCiudad.Text = "Ciudad:";
+            // 
+            // labelCantEstrellas
+            // 
+            this.labelCantEstrellas.AutoSize = true;
+            this.labelCantEstrellas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCantEstrellas.Location = new System.Drawing.Point(7, 100);
+            this.labelCantEstrellas.Name = "labelCantEstrellas";
+            this.labelCantEstrellas.Size = new System.Drawing.Size(126, 15);
+            this.labelCantEstrellas.TabIndex = 31;
+            this.labelCantEstrellas.Text = "Cantidad de Estrellas:";
+            // 
+            // labelDireccion
+            // 
+            this.labelDireccion.AutoSize = true;
+            this.labelDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDireccion.Location = new System.Drawing.Point(190, 153);
+            this.labelDireccion.Name = "labelDireccion";
+            this.labelDireccion.Size = new System.Drawing.Size(62, 15);
+            this.labelDireccion.TabIndex = 30;
+            this.labelDireccion.Text = "Direccion:";
+            // 
+            // labelTelefono
+            // 
+            this.labelTelefono.AutoSize = true;
+            this.labelTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTelefono.Location = new System.Drawing.Point(7, 154);
+            this.labelTelefono.Name = "labelTelefono";
+            this.labelTelefono.Size = new System.Drawing.Size(58, 15);
+            this.labelTelefono.TabIndex = 29;
+            this.labelTelefono.Text = "Telefono:";
+            // 
+            // labelMail
+            // 
+            this.labelMail.AutoSize = true;
+            this.labelMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMail.Location = new System.Drawing.Point(7, 128);
+            this.labelMail.Name = "labelMail";
+            this.labelMail.Size = new System.Drawing.Size(34, 15);
+            this.labelMail.TabIndex = 28;
+            this.labelMail.Text = "Mail:";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.Location = new System.Drawing.Point(7, 68);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(107, 15);
+            this.labelNombre.TabIndex = 27;
+            this.labelNombre.Text = "Nombre del Hotel:";
+            // 
+            // labelTituloDatos
+            // 
+            this.labelTituloDatos.AutoSize = true;
+            this.labelTituloDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTituloDatos.Location = new System.Drawing.Point(48, 34);
+            this.labelTituloDatos.Name = "labelTituloDatos";
+            this.labelTituloDatos.Size = new System.Drawing.Size(45, 17);
+            this.labelTituloDatos.TabIndex = 26;
+            this.labelTituloDatos.Text = "Datos";
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelTitulo.Location = new System.Drawing.Point(233, 6);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(135, 24);
+            this.labelTitulo.TabIndex = 25;
+            this.labelTitulo.Text = "Modificar Hotel";
+            // 
+            // botonQuitarRegimen
+            // 
+            this.botonQuitarRegimen.Location = new System.Drawing.Point(551, 235);
+            this.botonQuitarRegimen.Name = "botonQuitarRegimen";
+            this.botonQuitarRegimen.Size = new System.Drawing.Size(75, 23);
+            this.botonQuitarRegimen.TabIndex = 50;
+            this.botonQuitarRegimen.Text = "Quitar";
+            this.botonQuitarRegimen.UseVisualStyleBackColor = true;
+            // 
+            // ModificarHotel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 314);
+            this.Controls.Add(this.botonQuitarRegimen);
+            this.Controls.Add(this.textBoxPais);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.listBoxRegimenes);
+            this.Controls.Add(this.comboBoxTipoRegimen);
+            this.Controls.Add(this.dateFechaCreacion);
+            this.Controls.Add(this.textBoxCiudad);
+            this.Controls.Add(this.textBoxTelefono);
+            this.Controls.Add(this.textBoxMail);
+            this.Controls.Add(this.comboBoxCantEstrellas);
+            this.Controls.Add(this.textBoxNombreHotel);
+            this.Controls.Add(this.botonAgregarRegimen);
+            this.Controls.Add(this.botonVolver);
+            this.Controls.Add(this.botonLimpiar);
+            this.Controls.Add(this.botonRegistrar);
+            this.Controls.Add(this.labelTipoRegimen);
+            this.Controls.Add(this.labelFechaCreacion);
+            this.Controls.Add(this.labelPais);
+            this.Controls.Add(this.labelCiudad);
+            this.Controls.Add(this.labelCantEstrellas);
+            this.Controls.Add(this.labelDireccion);
+            this.Controls.Add(this.labelTelefono);
+            this.Controls.Add(this.labelMail);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelTituloDatos);
+            this.Controls.Add(this.labelTitulo);
+            this.Name = "ModificarHotel";
+            this.Text = "ModificarHotel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPais;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.ListBox listBoxRegimenes;
+        private System.Windows.Forms.ComboBox comboBoxTipoRegimen;
+        private System.Windows.Forms.DateTimePicker dateFechaCreacion;
+        private System.Windows.Forms.TextBox textBoxCiudad;
+        private System.Windows.Forms.TextBox textBoxTelefono;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.ComboBox comboBoxCantEstrellas;
+        private System.Windows.Forms.TextBox textBoxNombreHotel;
+        private System.Windows.Forms.Button botonAgregarRegimen;
+        private System.Windows.Forms.Button botonVolver;
+        private System.Windows.Forms.Button botonLimpiar;
+        private System.Windows.Forms.Button botonRegistrar;
+        private System.Windows.Forms.Label labelTipoRegimen;
+        private System.Windows.Forms.Label labelFechaCreacion;
+        private System.Windows.Forms.Label labelPais;
+        private System.Windows.Forms.Label labelCiudad;
+        private System.Windows.Forms.Label labelCantEstrellas;
+        private System.Windows.Forms.Label labelDireccion;
+        private System.Windows.Forms.Label labelTelefono;
+        private System.Windows.Forms.Label labelMail;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelTituloDatos;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Button botonQuitarRegimen;
+    }
+}
