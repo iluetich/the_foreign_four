fa4aea0bdb51b45908d7330a4dc2b1015821ce9c
aniPerezG <anitaperez2@gmail.com>
2014-10-17 15:02:40 -0300

    Termino la migracion a la tabla Reservas

--- Script SQL/Insercion Tablas.sql
+++ Script SQL/Insercion Tablas.sql
@@ -1,26 +1,26 @@
+--***CLIENTES********************************
 INSERT INTO THE_FOREIGN_FOUR.Clientes (nombre, apellido, fecha_nac, nom_calle, nro_calle, piso, depto, nacionalidad, nro_doc, mail)
 SELECT DISTINCT Cliente_Nombre, Cliente_Apellido, Cliente_Fecha_Nac, Cliente_Dom_Calle, Cliente_Nro_Calle, Cliente_Piso, Cliente_Depto, Cliente_Nacionalidad, Cliente_Pasaporte_Nro, Cliente_Mail
 FROM gd_esquema.Maestra
 
---*********************************************
+--***HOTELES***********************************
 
 INSERT INTO THE_FOREIGN_FOUR.Hoteles (nom_calle, ciudad, nro_calle, cant_estrellas, recarga_estrellas)
 SELECT DISTINCT Hotel_Calle, Hotel_Ciudad, Hotel_Nro_Calle, Hotel_CantEstrella, Hotel_Recarga_Estrella
 FROM gd_esquema.Maestra
 
---*********************************************
-
+--***REGIMENES********************************
 INSERT INTO THE_FOREIGN_FOUR.Regimenes (descripcion, precio)
 SELECT DISTINCT Regimen_Descripcion, Regimen_Precio
 FROM gd_esquema.Maestra
 
---*********************************************
+--***TIPO HABITACIONES***************************
 
 INSERT INTO THE_FOREIGN_FOUR.TipoHabitaciones (cod_tipo_hab, descripcion, recargo)
 SELECT DISTINCT Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion, Habitacion_Tipo_Porcentual
 FROM gd_esquema.Maestra
 
---*********************************************
+--***CONSUMIBLES**************************************
 
 INSERT INTO THE_FOREIGN_FOUR.Consumibles (cod_consumible, descripcion, precio)
 SELECT DISTINCT Consumible_Codigo, Consumible_Descripcion, Consumible_Precio
@@ -28,7 +28,7 @@ FROM gd_esquema.Maestra
 WHERE Consumible_Codigo IS NOT NULL
 AND Consumible_Descripcion IS NOT NULL
 
---*********************************************
+--***HABITACIONES**************************************
 
 INSERT INTO THE_FOREIGN_FOUR.Habitaciones (piso, ubicacion, cod_tipo_hab, nro_habitacion, cod_hotel)
 SELECT DISTINCT Habitacion_Piso,
@@ -44,7 +44,7 @@ SELECT DISTINCT Habitacion_Piso,
 			    AND		ho.recarga_estrellas = m.Hotel_Recarga_Estrella) AS 'cod_hotel'
 FROM gd_esquema.Maestra m
 
---*********************************************
+--***ESTADIAS***************************************
 
 INSERT INTO THE_FOREIGN_FOUR.Estadias (fecha_inicio, cant_noches, nro_habitacion, cod_reserva)
 SELECT DISTINCT  m.Estadia_Fecha_Inicio,
@@ -67,21 +67,23 @@ SELECT DISTINCT  m.Estadia_Fecha_Inicio,
 				 
 FROM gd_esquema.Maestra m
 
---*********************************************
+--***RESERVAS***************************************
 
-INSERT INTO THE_FOREIGN_FOUR.Reservas (cod_reserva, fecha_desde, cant_noches, cod_hotel, cod_cliente)
+INSERT INTO THE_FOREIGN_FOUR.Reservas	(cod_reserva, fecha_desde, cant_noches, cod_hotel, cod_cliente, cod_tipo_hab,
+										 cod_regimen, fecha_hasta)
 SELECT	DISTINCT m.Reserva_Codigo,
 		m.Reserva_Fecha_Inicio,
 		m.Reserva_Cant_Noches,
 		
-	   (SELECT cod_hotel
+	   (SELECT DISTINCT cod_hotel
 		FROM THE_FOREIGN_FOUR.Hoteles h
 		WHERE	h.nom_calle = m.Hotel_Calle
 		AND		h.nro_calle = m.Hotel_Nro_Calle
 		AND		h.ciudad = m.Hotel_Ciudad
 		AND		h.cant_estrellas = m.Hotel_CantEstrella
 		AND		h.recarga_estrellas = m.Hotel_Recarga_Estrella) AS 'cod_hotel',
-       (SELECT cod_cliente
+		
+       (SELECT DISTINCT cod_cliente
 		FROM THE_FOREIGN_FOUR.Clientes c
 		WHERE	c.nombre = m.Cliente_Nombre
 		AND		c.apellido = m.Cliente_Apellido
@@ -92,5 +94,20 @@ SELECT	DISTINCT m.Reserva_Codigo,
 		AND		c.nro_calle = m.Cliente_Nro_Calle
 		AND		c.piso = m.Cliente_Piso
 		AND		c.depto = m.Cliente_Depto
-		AND		c.nacionalidad = m.Cliente_Nacionalidad) AS 'cod_cliente'
+		AND		c.nacionalidad = m.Cliente_Nacionalidad) AS 'cod_cliente',
+		
+		m.Habitacion_Tipo_Codigo,
+		
+		(SELECT DISTINCT cod_regimen
+		FROM THE_FOREIGN_FOUR.Regimenes r
+		WHERE r.descripcion = m.Regimen_Descripcion
+		AND r.precio = m.Regimen_Precio) AS 'cod_regimen',
+		
+		--hay que hacer una funcion o procedure para establecer el codigo de reserva
+		
+		(m.Reserva_Fecha_Inicio + m.Reserva_Cant_Noches) AS 'fecha_hasta'
 FROM gd_esquema.Maestra m
+
+
+SELECT * 
+FROM THE_FOREIGN_FOUR.Reservas
