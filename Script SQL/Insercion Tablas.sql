0d2b0eb0801962b66acd9434185306018d1a3642
iluetich <ivan.luetich93@gmail.com>
2014-10-16 23:07:47 -0300

    Agrego inserción de Estadías
    # Falta crear tabla de Habitaciones para que funcione
    # Falta trigger sobre estadías defectuosas

--- Script SQL/Insercion Tablas.sql
+++ Script SQL/Insercion Tablas.sql
@@ -2,20 +2,41 @@ INSERT INTO THE_FOREIGN_FOUR.Clientes (nombre, apellido, fecha_nac, nom_calle, n
 SELECT DISTINCT Cliente_Nombre, Cliente_Apellido, Cliente_Fecha_Nac, Cliente_Dom_Calle, Cliente_Nro_Calle, Cliente_Piso, Cliente_Depto, Cliente_Nacionalidad, Cliente_Pasaporte_Nro, Cliente_Mail
 FROM gd_esquema.Maestra
 
+--*********************************************
+
 INSERT INTO THE_FOREIGN_FOUR.Hoteles (nom_calle, ciudad, nro_calle, cant_estrellas, recarga_estrellas)
 SELECT DISTINCT Hotel_Calle, Hotel_Ciudad, Hotel_Nro_Calle, Hotel_CantEstrella, Hotel_Recarga_Estrella
 FROM gd_esquema.Maestra
+
+--*********************************************
 
 INSERT INTO THE_FOREIGN_FOUR.Regimenes (descripcion, precio)
 SELECT DISTINCT Regimen_Descripcion, Regimen_Precio
 FROM gd_esquema.Maestra
 
+--*********************************************
+
 INSERT INTO THE_FOREIGN_FOUR.TipoHabitaciones (cod_tipo_hab, descripcion, recargo)
 SELECT DISTINCT Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion, Habitacion_Tipo_Porcentual
 FROM gd_esquema.Maestra
 
+--*********************************************
 
+INSERT INTO THE_FOREIGN_FOUR.Estadias (fecha_inicio, cant_noches)
+SELECT DISTINCT Estadia_Fecha_Inicio,
+				Estadia_Cant_Noches,
+			    (SELECT nro_habitacion
+				 FROM THE_FOREIGN_FOUR.Habitaciones ha JOIN THE_FOREIGN_FOUR.Hoteles ho ON(ha.cod_hotel = ho.cod_hotel)
+													   JOIN THE_FOREIGN_FOUR.TipoHabitaciones t ON(ha.cod_tipo_hab = t.cod_tipo_hab)
+				 WHERE  ha.nro_habitacion = m.Habitacion_Numero
+				 AND	ha.ubicacion = m.Habitacion_Frente
+				 AND	ha.cod_tipo_hab = m.Habitacion_Tipo_Codigo
+				 AND	ha.piso = m.Habitacion_Piso
+				 AND	t.recargo = m.Habitacion_Tipo_Porcentual
+				 AND	t.descripcion = m.Habitacion_Tipo_Descripcion)
+FROM gd_esquema.Maestra m
 
+--*********************************************
 
 INSERT INTO THE_FOREIGN_FOUR.Reservas (cod_reserva, fecha_desde, cant_noches, cod_hotel, cod_cliente )
 SELECT	DISTINCT m.Reserva_Codigo,
