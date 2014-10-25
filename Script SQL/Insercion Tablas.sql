fc057ee9c0fc123145a375bd7c0223557f131ac6
iluetich <ivan.luetich93@gmail.com>
2014-10-25 13:02:26 -0300

    elimino join prescindible

--- Script SQL/Insercion Tablas.sql
+++ Script SQL/Insercion Tablas.sql
@@ -109,7 +109,7 @@ SELECT DISTINCT	m.Factura_Nro,
 				m.Factura_Fecha,
 				m.Factura_Total,
 				(SELECT cod_estadia
-				FROM THE_FOREIGN_FOUR.Estadias e JOIN THE_FOREIGN_FOUR.Reservas r ON(e.cod_reserva = r.cod_reserva)
+				FROM THE_FOREIGN_FOUR.Estadias e
 				WHERE	e.cod_reserva = m.Reserva_Codigo)
 FROM gd_esquema.Maestra m
 
