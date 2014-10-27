432960830d02663fc4efd015917c416ba5226a6b
nachomf <nachoemf@hotmail.com>
2014-10-27 10:01:29 -0300

    empiezo a hacer menues

---
+++ FrbaHotel/FrbaHotel/Menues/Menues.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.Menues
+{
+    public partial class Menues : Form
+    {
+        public Menues(int codRol)
+        {
+            InitializeComponent();
+            generarMenu(codRol);
+        }
+
+
+        public void generarMenu(int rol)
+        {
+            List<string> funcionalidades = new List<string>();
+
+            //se consulta que funcionalidades tiene el rol
+
+            foreach (string funcionalida in funcionalidades)
+            {
+                if (funcionalida == "generar Reserva")
+                {
+                    this.generarBoton(
+                }
+
+
+            }
+
+
+        }
+    
+    
+    }
+}
+
