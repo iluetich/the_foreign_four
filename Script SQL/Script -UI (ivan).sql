239f3e55fc3cd76c70b4c9abd605955c7744fd0f
iluetich <ivan.luetich93@gmail.com>
2014-10-27 01:47:50 -0300

    Agrego procedure registrar_huesped
    # También modifico nombre de funcion obtener_huespedes

--- Script SQL/Script -UI (ivan).sql
+++ Script SQL/Script -UI (ivan).sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION	THE_FOREIGN_FOUR.func_obtener_huespedes
+CREATE FUNCTION	THE_FOREIGN_FOUR.func_obtener_cant_huespedes
 				(@cod_reserva numeric(18,0))
 				
 RETURNS TABLE
@@ -7,4 +7,13 @@ RETURN (SELECT	COUNT(c.cod_cliente) AS cantidad_huespedes
 		FROM THE_FOREIGN_FOUR.Clientes c JOIN THE_FOREIGN_FOUR.ClientePorEstadia cpe ON(c.cod_cliente = cpe.cod_cliente)
 										 JOIN THE_FOREIGN_FOUR.Estadias e ON(cpe.cod_estadia = e.cod_estadia)
 		WHERE @cod_reserva = e.cod_reserva)
+		
+CREATE PROCEDURE THE_FOREIGN_FOUR.proc_registrar_huesped
+				(@cod_cliente numeric(18,0),
+				 @cod_estadia numeric(18,0))
+AS
+	INSERT INTO ClientePorEstadia (cod_cliente, cod_estadia)
+	VALUES	(@cod_cliente, @cod_estadia)
+GO
+
 
