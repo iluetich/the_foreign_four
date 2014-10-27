e61b8302d9e7ac2485bbbc4802a9de9e84e5fa5a
iluetich <ivan.luetich93@gmail.com>
2014-10-27 01:31:28 -0300

    Corrijo semántica de la function obtener_huespedes

--- Script SQL/Script -UI (ivan).sql
+++ Script SQL/Script -UI (ivan).sql
@@ -3,7 +3,9 @@ CREATE FUNCTION	THE_FOREIGN_FOUR.func_obtener_huespedes
 				
 RETURNS TABLE
 AS
-RETURN (SELECT	c.cod_cliente, c.nombre, c.apellido, c.mail, c.tipo_doc, c.nro_doc, c.fecha_nac
+RETURN (SELECT	COUNT(c.cod_cliente)
 		FROM THE_FOREIGN_FOUR.Clientes c JOIN THE_FOREIGN_FOUR.ClientePorEstadia cpe ON(c.cod_cliente = cpe.cod_cliente)
 										 JOIN THE_FOREIGN_FOUR.Estadias e ON(cpe.cod_estadia = e.cod_estadia)
-		WHERE 46005 = e.cod_reserva)
+		WHERE @cod_reserva = e.cod_reserva)
+		
+
