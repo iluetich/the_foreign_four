0c499128de5c6314222fd9e3281c4896196e8e55
Ian Cincotta <iancincotta@gmail.com>
2014-12-25 01:14:26 -0300

    agrego levantar fecha por archivo de configuracion

---
+++ FrbaHotel/FrbaHotel/DateConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaHotel
+{
+    class DateConfig{        
+
+        internal static String GetTime(){
+            
+            //Devuele la fecha seteada en el app.config
+            return Properties.Settings.Default.SystemDate.ToString("yyyy-dd-MM");
+        }             
+    }
+}
